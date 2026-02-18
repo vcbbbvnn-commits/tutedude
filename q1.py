b4042a4f0c90915be4550bc7f75a0cabfbaf0813
Shubhayush DN <vcbbbvnn@gmail.com>
2026-02-18 13:22:16 +0530

    2

---
+++ q1.py
@@ -0,0 +1,15 @@
+# Take two numbers as input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2 
+
+
+print("\nResults:")
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
